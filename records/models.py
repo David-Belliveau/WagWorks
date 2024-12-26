1a481232030635cd21fac86af1de27aa32d8fa48
David Belliveau <dibsy@davids-mbp.mynetworksettings.com>
2024-12-25 23:27:59 -0500

    Update models for Pet feature

--- records/models.py
+++ records/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Pet(models.Model):
+    # user = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     related_name="pet"
+    #     )
+    email = models.EmailField(max_length=254, null=True, blank=True)
+    birthday = models.DateTimeField()
+    passing_date = models.DateTimeField()
+    breed = models.TextField(default="Default content")
+    diagnosis_date = models.DateTimeField()
+    vaccinations = models.TextField()
+    rbc_1 = models.IntegerField()
+    pcv_1 = models.IntegerField()
+    ret_1 = models.IntegerField()
+    b_trans1 = models.BooleanField()
+    prescriptions = models.TextField()
