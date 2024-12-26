6de9f217172e2d07a91d189eec7652c74d892225
David Belliveau <dibsy@davids-mbp.mynetworksettings.com>
2024-12-26 16:45:10 -0500

    Early draft of landing page, navbar, color palette, and updated general infrastructure.

--- records/urls.py
+++ records/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("records/register.html/", views.register, name="register"),
+    path("records/about.html/", views.about, name="about"),
 
 ]
