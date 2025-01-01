92a28deeb307447f4048b5ec5cd7e5df570a665c
David Belliveau <dibsy@davids-mbp.mynetworksettings.com>
2025-01-01 17:05:56 -0500

    Refactor website layout and index page

--- records/urls.py
+++ records/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-
 from . import views
+# from .views import MultiStepFormView
+
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("register.html/", views.register, name="register"),
     path("about.html/", views.about, name="about"),
-    path("success.html/", views.success, name="success")
-
+    path("success.html/", views.success, name="success"),
+    path("mission.html/", views.mission, name="mission"),
+    path("register2.html/", views.register2, name="register2"),
+    # path('register2/', RegistrationWizard.as_view(forms), name='register2')
 ]
+
+
--- records/views.py
+++ records/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import PetDetailsForm
+from django.http import HttpResponseRedirect
+
+
 
 # Create your views here.
 def index(request):
@@ -28,9 +31,34 @@ def register(request):
     return render(request, "records/register.html", {'form': form})
 
 
+
+def register2(request):
+    if request.method == "POST":
+        print("POST request received")  # Debugging log
+        form = PetDetailsForm(request.POST)
+        if form.is_valid():
+            print("Form is valid")  # Debugging log
+            form.save() # Save data to the database
+            return redirect('success')
+        else:
+            print("Form is invalid")  # Debugging log
+            print(form.errors)
+            return render(request, "records/register2.html", {'form': form})
+
+
+    else:
+        form = PetDetailsForm()
+    
+    return render(request, "records/register2.html", {'form': form})
+
+
 def about(request):
     return render(request, "records/about.html")
 
 
 def success(request):
     return render(request, "records/success.html")
+
+
+def mission(request):
+    return render(request, "records/mission.html")
